a9ba2c264dd1844843b1db86bf3650262ff05a1a
HojinPark <60504706+1ho0jin1@users.noreply.github.com>
2022-04-05 17:01:22 +0900

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,15 @@ data_config = {
         "G_data_dir": "/home/yoon/datasets/face/CASIA_clean/cropped_images",  # CASIA-WebFace/cropped_images directory
         "K_data_dir": "/home/yoon/datasets/face/CASIA_clean/cropped_images",  # CASIA-WebFace/cropped_images directory
         "U_data_dir": "/home/yoon/datasets/face/CASIA_clean/cropped_images",  # CASIA-WebFace/cropped_images directory
-        "known_pkl": "/home/yoon/datasets/face/CASIA_clean/known_list_5000.pkl",  # CASIA_known_list.pkl directory
-        "unknown_pkl": "/home/yoon/datasets/face/CASIA_clean/unknown_list_5000.pkl",  #  CASIA_unknown_list.pkl directory
+        "known_pkl": "/home/yoon/datasets/face/CASIA_clean/CASIA_known_list.pkl",  # CASIA_known_list.pkl directory
+        "unknown_pkl": "/home/yoon/datasets/face/CASIA_clean/CASIA_unknown_list.pkl",  #  CASIA_unknown_list.pkl directory
     },
     "IJBC":{
         "G_data_dir" : "/home/yoon/datasets/face/IJB/IJB_C_Cropped/images_organized",  # IJBC/images_organized directory
         "K_data_dir" : "/home/yoon/datasets/face/IJB/IJB_C_Cropped/frames_organized",  # IJBC/frames_organized directory
         "U_data_dir" : "/home/yoon/datasets/face/IJB/IJB_C_Cropped/frames_organized",  # IJBC/frames_organized directory
-        "known_pkl" : "/home/yoon/datasets/face/IJB/IJB_C_Cropped/known_list_1765.pkl",  # IJBC_known_list.pkl directory
-        "unknown_pkl" : "/home/yoon/datasets/face/IJB/IJB_C_Cropped/unknown_list_1765.pkl",  # IJBC_unknown_list.pkl directory
+        "known_pkl" : "/home/yoon/datasets/face/IJB/IJB_C_Cropped/IJBC_known_list.pkl",  # IJBC_known_list.pkl directory
+        "unknown_pkl" : "/home/yoon/datasets/face/IJB/IJB_C_Cropped/IJBC_unknown_list.pkl",  # IJBC_unknown_list.pkl directory
     },
 }
 
