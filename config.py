79ffd8eb981614a658240aba206cae545d9a2749
HojinPark <60504706+1ho0jin1@users.noreply.github.com>
2023-11-12 01:22:38 +0900

    Update config.py
    Edited for reproducibility

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 data_config = {
     "CASIA": {
-        "image_directory": "/home/yoon/datasets/face/CASIA_clean/CASIA_subset"
+        "image_directory": "C:\\Users\\USER\\Downloads\\cropped_images",
+        "known_list_path":"C:\\Users\\USER\\Downloads\\CASIA_known_list.pkl",
+        "unknown_list_path":"C:\\Users\\USER\\Downloads\\CASIA_unknown_list.pkl",
     },
     "IJBC":{
         "image_directory" : ""
